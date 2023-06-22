ebd7c0c1d898a2274a2dddf588a54d3517ddc48c
Felipe Almeida Lucas <felipe.lucas@PRODATAMOBILITY.COM.BR>
2023-06-22 18:04:36 -0300

    script insert

---
+++ INSERT_PILOTOS.sql
@@ -0,0 +1,26 @@
+INSERT INTO Pilotos(
+PIL_NOME, #varchar
+PIL_SOBRENOME, #varchar
+EQI_EQUIPE, #int
+PIL_PODIOS, #int
+PIL_PONTOS_TOTAIS, #int
+PIL_CORRIDAS_OFICIAIS, #int
+PIL_CAMPEAO, #varchar
+PIL_MELHOR_CHEGADA, #varchar
+STA_ATIVO, #int
+PIL_ULTIMA_TEMPORADA, #int
+PIL_VOLTAS_RAPIDAS #int
+)
+VALUES(
+"Felipe", #PIL_NOME - nome do piloto
+"Almeida", #PIL_SOBRENOME - sobrenome do piloto
+1, #PIL_EQUIPE - id da equipe
+10, #PIL_PODIOS - número de pódios
+100, #PIL_PONTOS_TOTAIS - pontos totais conquistados
+20, #PIL_CORRIDAS_OFICIAIS - total de corridas disputadas
+"1x", #PIL_CAMPEAO - quantas vezes foi campeão
+"1 (2x)", #PIL_MELHOR_CHEGADA - melhor chegada do piloto
+1, #PIL_ATIVO - se o piloto está ativo ou não
+4, #PIL_ULTIMA_TEMPORADA - última temporada que o piloto se inscreveu
+5 #PIL_VOLTAS_RAPIDAS - quantas voltas rápidas o piloto fez na carreira
+)
